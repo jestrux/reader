aaca720ca97691194546843b64320efba16a6802
Walter Kimaro <wakyj07@gmail.com>
2024-02-13 21:21:25 +0300

    Clean up electron setup

--- electron/index.js
+++ electron/index.js
@@ -4,13 +4,14 @@ const { ipcMain, shell } = require("electron");
 
 const mb = menubar({
 	icon: path.join(__dirname, "icon.png"),
+	dir: path.join(__dirname, "build"),
 	browserWindow: {
 		width: 480,
 		height: 600,
 		backgroundColor: "#000000",
 		webPreferences: {
 			// nodeIntegration: true,
-			preload: path.join(__dirname, "preload.cjs"),
+			preload: path.join(__dirname, "preload.js"),
 		},
 	},
 	preloadWindow: true,
--- public/preload.cjs
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require("electron");
-
-window.addEventListener("open-url", (e) => {
-	ipcRenderer.send("open-url", e.detail);
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-	document.body.classList.add("on-electron");
-});
