3b27e780512b3b6fb6b6f67f071c28eb91819c2d
Walter Kimaro <wakyj07@gmail.com>
2024-03-05 12:32:20 +0300

    Update icons and dependencies

--- docs/assets/web-SBWNKJWP-U1DsqQ06.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-j8DRtXr7.js";var t=class extends e{constructor(){super()}async checkSendIntentReceived(){return{title:null}}finish(){}};export{t as SendIntentWeb};
---
+++ docs/assets/web-SBWNKJWP-bOwWrgPm.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-e4RxKl8Z.js";var t=class extends e{constructor(){super()}async checkSendIntentReceived(){return{title:null}}finish(){}};export{t as SendIntentWeb};
