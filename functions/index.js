b4facebc2edae908d782335774e5864c00c94ab5
Walter Kimaro <wakyj07@gmail.com>
2024-04-10 09:16:50 +0300

    Replace custom link preview logic with 'api.linkpreview.net'

--- functions/index.js
+++ functions/index.js
@@ -4,81 +4,86 @@ import * as admin from "./admin.js";
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
-import { parse } from "node-html-parser";
+// import { parse } from "node-html-parser";
 
-function processWebsite(url, content) {
-	const div = parse(content);
+async function processWebsite(url, content) {
+	return fetch(`https://api.linkpreview.net/?q=${url}`, {
+		headers: {
+			"X-Linkpreview-Api-Key": "b7fc8791125983bdf0b831273964f8b1",
+		},
+	}).then((res) => res.json());
+	// const div = parse(content);
 
-	const ogImage = div
-		.querySelector(`[property="og:image"]`)
-		?.getAttribute("content");
+	// const ogImage = div
+	// 	.querySelector(`[property="og:image"]`)
+	// 	?.getAttribute("content");
 
-	let shortCutIcon = div
-		.querySelector(`[rel="shortcut icon"]`)
-		?.getAttribute("href");
+	// let shortCutIcon = div
+	// 	.querySelector(`[rel="shortcut icon"]`)
+	// 	?.getAttribute("href");
 
-	if (shortCutIcon && shortCutIcon.toString().charAt(0) == "/") {
-		let baseUrl = new URL(url).href;
-		if (baseUrl.endsWith("/")) {
-			baseUrl = baseUrl.substring(0, baseUrl.length - 1);
-		}
-		shortCutIcon = baseUrl + "/" + shortCutIcon.substring(1);
-	}
+	// if (shortCutIcon && shortCutIcon.toString().charAt(0) == "/") {
+	// 	let baseUrl = new URL(url).href;
+	// 	if (baseUrl.endsWith("/")) {
+	// 		baseUrl = baseUrl.substring(0, baseUrl.length - 1);
+	// 	}
+	// 	shortCutIcon = baseUrl + "/" + shortCutIcon.substring(1);
+	// }
 
-	const appleTouchIcon = div
-		.querySelector(`[rel="apple-touch-icon"]`)
-		?.getAttribute("href");
+	// const appleTouchIcon = div
+	// 	.querySelector(`[rel="apple-touch-icon"]`)
+	// 	?.getAttribute("href");
 
-	const twitterImage = div
-		.querySelector(`[name="twitter:image"]`)
-		?.getAttribute("content");
+	// const twitterImage = div
+	// 	.querySelector(`[name="twitter:image"]`)
+	// 	?.getAttribute("content");
 
-	const title = div.querySelector(`title`)?.textContent;
-	const ogTitle = div
-		.querySelector(`[property="og:title"]`)
-		?.getAttribute("content");
-	const twitterTitle = div
-		.querySelector(`[name="twitter:title"]`)
-		?.getAttribute("content");
+	// const title = div.querySelector(`title`)?.textContent;
+	// const ogTitle = div
+	// 	.querySelector(`[property="og:title"]`)
+	// 	?.getAttribute("content");
+	// const twitterTitle = div
+	// 	.querySelector(`[name="twitter:title"]`)
+	// 	?.getAttribute("content");
 
-	const description = div
-		.querySelector(`[property="description"]`)
-		?.getAttribute("content");
+	// const description = div
+	// 	.querySelector(`[property="description"]`)
+	// 	?.getAttribute("content");
 
-	const ogDescription = div
-		.querySelector(`[property="og:description"]`)
-		?.getAttribute("content");
+	// const ogDescription = div
+	// 	.querySelector(`[property="og:description"]`)
+	// 	?.getAttribute("content");
 
-	const twitterDescription = div
-		.querySelector(`[name="twitter:description"]`)
-		?.getAttribute("content");
+	// const twitterDescription = div
+	// 	.querySelector(`[name="twitter:description"]`)
+	// 	?.getAttribute("content");
 
-	return {
-		url,
-		image:
-			[
-				...new Set(
-					[
-						twitterImage,
-						ogImage,
-						appleTouchIcon,
-						shortCutIcon,
-					].filter((v) => v)
-				),
-			]?.[0] ?? null,
-		title:
-			[
-				...new Set([twitterTitle, ogTitle, title].filter((v) => v)),
-			]?.[0] ?? null,
-		description:
-			[
-				...new Set(
-					[twitterDescription, ogDescription, description].filter(
-						(v) => v
-					)
-				),
-			]?.[0] ?? null,
-	};
+	// return {
+	// 	url,
+	// 	image:
+	// 		[
+	// 			...new Set(
+	// 				[
+	// 					twitterImage,
+	// 					ogImage,
+	// 					appleTouchIcon,
+	// 					shortCutIcon,
+	// 				].filter((v) => v)
+	// 			),
+	// 		]?.[0] ?? null,
+	// 	title:
+	// 		[
+	// 			...new Set([twitterTitle, ogTitle, title].filter((v) => v)),
+	// 		]?.[0] ?? null,
+	// 	description:
+	// 		[
+	// 			...new Set(
+	// 				[twitterDescription, ogDescription, description].filter(
+	// 					(v) => v
+	// 				)
+	// 			),
+	// 		]?.[0] ?? null,
+	// };
 }
 
 const app = express();
@@ -88,7 +93,7 @@ app.use(cors({ origin: "*" }));
 app.get("/crawl/:url", async (req, res) => {
 	const response = await fetch(req.params.url);
 	const data = await response.text();
-	const meta = processWebsite(req.params.url, data);
+	const meta = await processWebsite(req.params.url, data);
 
 	res.status(200).json({ success: true, url: req.params.url, data, meta });
 });
@@ -97,7 +102,7 @@ app.post("/crawl", async (req, res) => {
 	const url = req.body.url;
 	const response = await fetch(url);
 	const content = await response.text();
-	const entry = processWebsite(url, content);
+	const entry = await processWebsite(url, content);
 
 	const collectionRef = admin.db.collection("reader");
 	const entries = (await collectionRef.get()).docs;
